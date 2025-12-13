4ff670cb4a295ad14276edc03a9df9fecc4aab9a
RomanF3 <romanfedorgithub@gmail.com>
2025-12-13 23:07:12 +0100

    color palety v colors

--- lib/theme/colors.dart
+++ lib/theme/colors.dart
@@ -7,3 +7,35 @@ class AppColors {
   static const Color accent = Color(0xFF38BDF8);
   static const Color text = Color(0xFF1F2937); 
 }
+
+class AppColorsRed {
+  static const Color background = Color(0xFFFFFFF9);
+  static const Color primary = Color(0xFFF24D41);
+  static const Color secondary = Color(0xFF8F352D);
+  static const Color accent = Color(0xFFC23F36);
+  static const Color text = Color(0xFF332625); 
+}
+
+class AppColorsGreen {
+  static const Color background = Color(0xFFF9FFF4);
+  static const Color primary = Color(0xFF17F54B);
+  static const Color secondary = Color(0xFF468F57);
+  static const Color accent = Color(0xFF38C258);
+  static const Color text = Color(0xFF19181C); 
+}
+
+class AppColorsOrange {
+  static const Color background = Color(0xFFFFFAF0);
+  static const Color primary = Color(0xFFF48656);
+  static const Color secondary = Color(0xFFE38D36);
+  static const Color accent = Color(0xFFF5810B);
+  static const Color text = Color(0xFF191714); 
+}
+
+class AppColorsTurquise {
+  static const Color background = Color(0xFFF5FFFC);
+  static const Color primary = Color(0xFF00FAB3);
+  static const Color secondary = Color(0xFF28C79A);
+  static const Color accent = Color(0xFF3B947B);
+  static const Color text = Color(0xFF131918); 
+}
